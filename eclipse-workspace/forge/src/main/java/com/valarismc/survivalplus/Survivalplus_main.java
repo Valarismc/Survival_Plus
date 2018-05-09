8204c7219f540c9c4d66671fe93be2d00c2ad247
Valarismc <valarismc@gmail.com>
2018-05-08 20:01:51 -0400

    first commit

---
+++ eclipse-workspace/forge/src/main/java/com/valarismc/survivalplus/Survivalplus_main.java
@@ -0,0 +1,20 @@
+package com.valarismc.survivalplus;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Survivalplus_main.MODID, version = Survivalplus_main.VERSION)
+public class Survivalplus_main
+{
+    public static final String MODID = "Survival Plus";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
